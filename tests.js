98b10eb701230dbf7cb806a00b03bd96e861bf8a
DANCAN-OPENJI-DEVELOPS <114863301+DANCAN-OPENJI-DEVELOPS@users.noreply.github.com>
2022-10-31 16:19:22 +0300

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,5 @@
+//myTests
+rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+Passed:rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+Passed:rot13("SERR YBIR?") should decode to the string FREE LOVE?
+Passed:rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
